0af96337121235d86b30cac31e88a31ef44c2fb3
Kirk Baguley <d00329932@dmail.dixie.edu>
2021-04-19 09:40:38 -0600

    from php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php include_once('website/home.html'); 
 echo '<link href="website/style.css" rel="stylesheet">';
 echo '<script type="text/javascript" src="website/app.js"></script>';
+shell_exec('python3 ./perspectiveserver.py')
 ?>
